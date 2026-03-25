131c361214d4ce772c762415330e8253fcbc456d
Julius Korbjuhn <julius@korbjuhn.net>
2026-03-25 23:03:41 +0100

    Adding Tracing for the backend

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -3,12 +3,18 @@ extern crate rocket;
 use backend::{init_db, routes::login_request};
 use rocket::{Config, Rocket, get};
 use std::net::{IpAddr, Ipv4Addr};
+use tracing_subscriber::{
+    FmtSubscriber,
+    fmt::format::{Compact, FmtSpan, Format, JsonFields},
+};
 
+#[tracing::instrument]
 #[get("/health")]
 fn health() -> &'static str {
     "Healthy"
 }
 
+#[tracing::instrument]
 #[get("/hi")]
 fn hi() -> String {
     "Hello, world!".to_string()
@@ -29,7 +35,18 @@ async fn main() -> Result<(), rocket::Error> {
 
     init_db().await;
 
-    let rocket: Rocket<rocket::Ignite> = rocket::build()
+    let subscriber: FmtSubscriber<JsonFields, Format<Compact>> = tracing_subscriber::fmt()
+        .json()
+        .compact()
+        .with_file(true)
+        .with_line_number(true)
+        .with_target(false)
+        .with_span_events(FmtSpan::ENTER | FmtSpan::CLOSE)
+        .finish();
+
+    tracing::subscriber::set_global_default(subscriber).expect("Failed to set global subscriber");
+
+    let _rocket: Rocket<rocket::Ignite> = rocket::build()
         .configure(server_config)
         .mount("/", routes![hi, health, login_request])
         .launch()
--- backend/src/routes/login.rs
+++ backend/src/routes/login.rs
@@ -4,9 +4,9 @@ use std::sync::atomic::{AtomicPtr, Ordering};
 
 use argon2::{PasswordHash, PasswordVerifier};
 use rocket::http::Status;
-use rocket::tokio::task::{JoinHandle, spawn_blocking};
 use rocket::post;
 use rocket::serde::json::Json;
+use rocket::tokio::task::{JoinHandle, spawn_blocking};
 use sqlx::{Error, MySql, Pool, Row};
 
 use crate::data_definitions::UserLoginRequest;
@@ -16,23 +16,24 @@ const LOGIN_QUERY_STR: &str = r#"
 SELECT password AS password_hash FROM users WHERE email = ? LIMIT 1;
 "#;
 
-
 /// This is highly dependant on knowing of where the data lives in this context this is safe, but it can not be made public due to hard constraints
 struct SendSliceAcrossThreads<T> {
     ptr: AtomicPtr<T>,
     len: usize,
 }
 
-impl <T: Sized> SendSliceAcrossThreads<T> {
+impl<T: Sized> SendSliceAcrossThreads<T> {
     pub fn from(value: &[T]) -> Self {
-        Self { ptr: AtomicPtr::new(value.as_ptr().cast_mut()), len: value.len() }
+        Self {
+            ptr: AtomicPtr::new(value.as_ptr().cast_mut()),
+            len: value.len(),
+        }
     }
 
     pub fn get(&self) -> &[T] {
         unsafe { slice::from_raw_parts(self.ptr.load(Ordering::Relaxed), self.len) }
     }
 }
-
 
 #[post("/login", format = "json", data = "<login_request>")]
 pub async fn login(login_request: Json<UserLoginRequest<'_>>) -> (Status, &'static str) {
@@ -45,30 +46,34 @@ pub async fn login(login_request: Json<UserLoginRequest<'_>>) -> (Status, &'stat
 
     match result {
         Ok(Some(row)) => {
-            let login_password: SendSliceAcrossThreads<u8> = SendSliceAcrossThreads::from(login_request.0.password.as_bytes());
+            let login_password: SendSliceAcrossThreads<u8> =
+                SendSliceAcrossThreads::from(login_request.0.password.as_bytes());
 
-            let task: JoinHandle<(Status, &str)> = spawn_blocking(move || -> (Status, &'static str) {
-                let hash_str: &str = match row.try_get("password_hash") {
-                    Ok(h) => h,
-                    Err(_) => return (Status::InternalServerError, "Internal error"),
-                };
+            let task: JoinHandle<(Status, &str)> =
+                spawn_blocking(move || -> (Status, &'static str) {
+                    let hash_str: &str = match row.try_get("password_hash") {
+                        Ok(h) => h,
+                        Err(_) => return (Status::InternalServerError, "Internal error"),
+                    };
 
-                let parsed_hash: PasswordHash = match PasswordHash::new(&hash_str) {
-                    Ok(h) => h,
-                    Err(err) => return {
-                        dbg!(err);
-                        (Status::InternalServerError, "Internal error")
-                    },
-                };
-                
-                match ARGON_2.verify_password(login_password.get(), &parsed_hash) {
-                    Ok(_) => (Status::Ok, "OK"),
-                    Err(err) => {
-                        dbg!(err);
-                        (Status::Unauthorized, "Invalid credentials")
+                    let parsed_hash: PasswordHash = match PasswordHash::new(&hash_str) {
+                        Ok(h) => h,
+                        Err(err) => {
+                            return {
+                                dbg!(err);
+                                (Status::InternalServerError, "Internal error")
+                            };
+                        }
+                    };
+
+                    match ARGON_2.verify_password(login_password.get(), &parsed_hash) {
+                        Ok(_) => (Status::Ok, "OK"),
+                        Err(err) => {
+                            dbg!(err);
+                            (Status::Unauthorized, "Invalid credentials")
+                        }
                     }
-                }
-            });
+                });
 
             let result: (Status, &'static str) = task.await.unwrap();
             result
