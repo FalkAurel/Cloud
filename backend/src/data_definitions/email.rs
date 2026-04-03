f0300f94a3d5791077f81ffa4b0fc7450d1b3523
Julius Korbjuhn <julius.korbjuhn@sap.com>
2026-04-03 13:42:02 +0200

    Fixing Testcase

--- backend/src/data_definitions/email.rs
+++ backend/src/data_definitions/email.rs
@@ -13,7 +13,7 @@ use lettre::{
 };
 
 #[derive(Debug)]
-pub(crate) enum EmailError {
+pub enum EmailError {
     EmailError(PrivateEmailError),
     SendError(PrivateSendError),
     AddressError(AddressError),
@@ -170,7 +170,7 @@ mod tests {
 
     #[test]
     fn invalid_email_no_local_rejected() {
-        assert!("@example.com".parse::<Address>().is_ok());
+        assert!("@example.com".parse::<Address>().is_err());
     }
 
     #[test]
